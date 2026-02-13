c96d49debd7ab3e1d12dcde6e04274c18a14c4e7
Brianna Trankle <bt543@drexel.edu>
2026-02-13 10:48:25 -0500

    added readme and citation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # UbiqCore
+
+UbiqCore is a protein structure resource created to help advance ubiquitin ligase research. UbiqCore provides ubiquitin-E2-E3 ternary complex structures generated using ColabFold for combinations of known E2 enzymes and RING/U-box E3 ligases. ColabFold models are available for the complexes along with prediction confidence metrics, additional structure analysis data, and final predicted pair probability. See the live site  at: https://dunbrack.fccc.edu/ubiqcore
+
+This repository contains the code powering the site (a Streamlit application).
+
+See our preprint:
+- Jarboe, B., & Dunbrack, R. (2026). Structure–guided analysis and prediction of human E2–E3 ligase pairing specificity (p. 2026.02.10.700855). bioRxiv. https://doi.org/10.64898/2026.02.10.700855
+
--- pages/Home.py
+++ pages/Home.py
@@ -9,10 +9,12 @@ col1, col2 = st.columns([1, 1])
 with col1:
     st.write(' ')
     st.header('Welcome to UbiqCore!')
-    st.markdown('UbiqCore is a protein structure resource created to help advance ubiquitin ligase research. UbiqCore provides ubiquitin-E2-E3 ternary complex structures generated using ColabFold for combinations of known E2 enzymes and RING/U-box E3 ligases. ColabFold models are available for the complexes along with prediction confidence metrics, additional structure analysis data, and final predicted pair probability. Choose from the menu above to browse structure data and/or download .pdb files of ternary complexes of interest. We also provide a listing of experimental structures which contain E2 enzymes and/or E3 ligases available within the Protein Data Bank (PDB). See our paper for full details on the methods used and the data available in UbiqCore.')
+    st.markdown('UbiqCore is a protein structure resource created to help advance ubiquitin ligase research. UbiqCore provides ubiquitin-E2-E3 ternary complex structures generated using ColabFold for combinations of known E2 enzymes and RING/U-box E3 ligases. ColabFold models are available for the complexes along with prediction confidence metrics, additional structure analysis data, and final predicted pair probability. Choose from the menu above to browse structure data and/or download .pdb files of ternary complexes of interest. We also provide a listing of experimental structures which contain E2 enzymes and/or E3 ligases available within the Protein Data Bank (PDB). See our [preprint paper](https://www.biorxiv.org/content/10.64898/2026.02.10.700855) for full details on the methods used and the data available in UbiqCore.')
 
 with col2:
     show_3D_struc('util/resources/UB-E2-E3.pdb')
 
 
+st.write(' ')
+st.markdown('Jarboe, B., & Dunbrack, R. (2026). Structure–guided analysis and prediction of human E2–E3 ligase pairing specificity (p. 2026.02.10.700855). bioRxiv. https://doi.org/10.64898/2026.02.10.700855')
 
